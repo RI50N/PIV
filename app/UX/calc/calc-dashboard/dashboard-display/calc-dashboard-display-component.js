54899787667f37dbe6ea70332c54e01bb0f32c9b
Adriano Boese <desenvacc@gmail.com>
2017-08-31 18:01:30 -0300

    Calcula BTC

--- app/UX/calc/calc-dashboard/dashboard-display/calc-dashboard-display-component.js
+++ app/UX/calc/calc-dashboard/dashboard-display/calc-dashboard-display-component.js
@@ -13,15 +13,34 @@
 
     self.$onInit = onInit;
     self.mes = [];
+    self.hashrate = 0;
+    self.maintenanceFeed = 0.0035;
+    self.btcPerMhs = 0.02
+    self.dados = [];
+    self.dados['montante'] = 0;
 
     function onInit() {
       _populaMeses();
+      _justDoIt();
+    }
 
-      console.log(self.mes);
+    function _justDoIt() {
+        for (let i = 9; i < self.mes.length; i++) {
+          console.log(self.mes);
+          for (let j = 0; j < self.mes[i].length; j++) {
+            // self.dados[i][j]['data'] = new Date(2017, i, j);
+            self.dados['montante'] += ((self.hashrate/10)*self.btcPerMhs);
+            console.log(self.dados['montante']);
+            if (self.mes[i][j]==5) {
+              self.hashrate += 400;
+
+            }
+          }
+        }
     }
 
     function _populaMeses() {
-      for (let i = 1; i < 13; i++) {
+      for (let i = 9; i < 13; i++) {
         self.mes[i] = _getDiasMes(i, 2017);
       }
     }
