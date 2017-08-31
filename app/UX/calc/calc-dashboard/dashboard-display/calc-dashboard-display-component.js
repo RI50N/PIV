dda7c0a6683906c79a6a60b7c8ddeb449df825c3
Adriano Boese <desenvacc@gmail.com>
2017-08-31 15:43:46 -0300

    Listar dias do ano

--- app/UX/calc/calc-dashboard/dashboard-display/calc-dashboard-display-component.js
+++ app/UX/calc/calc-dashboard/dashboard-display/calc-dashboard-display-component.js
@@ -10,5 +10,31 @@
 
   function Controller() {
     var self = this;
+
+    self.$onInit = onInit;
+    self.mes = [];
+
+    function onInit() {
+      _populaMeses();
+
+      console.log(self.mes);
+    }
+
+    function _populaMeses() {
+      for (let i = 1; i < 13; i++) {
+        self.mes[i] = _getDiasMes(i, 2017);
+      }
+    }
+
+    function _getDiasMes(month, year) {
+      month--;
+      var date = new Date(year, month, 1);
+      var days = [];
+      while (date.getMonth() === month) {
+        days.push(date.getDate());
+        date.setDate(date.getDate() + 1);
+      }
+      return days;
+    }
   }
 }());
